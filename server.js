039b66957504e4b59efba442e4e993700e7a2376
Curve User <user@curve.local>
2026-01-08 15:07:39 +0530

    Fix: Final forced natural teeth color for all files (STL/OBJ/PLY) in 3D compare

--- server.js
+++ server.js
@@ -28,6 +28,12 @@ app.use(cors());
 app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
+// Force browser to always get the latest version (Cache Buster)
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+  next();
+});
+
 // Serve Static Files
 app.use(express.static(__dirname));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
